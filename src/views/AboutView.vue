b35336f34baa8d08d9b39d4e8912f31b69ddf98d
ryan <ryanscode@163.com>
2022-07-23 18:52:30 +0800

    [0723] add and test ant-desing-vue lib

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ msg }}</p>
+    <a-button type="primary">新增</a-button>
   </div>
 </template>
+<script lang="ts">
+import { Button, ButtonGroup } from "ant-design-vue";
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+    AButton: Button,
+    AButtonGroup: ButtonGroup,
+  },
+})
+export default class HomeView extends Vue {
+  msg = 1111;
+}
+</script>
