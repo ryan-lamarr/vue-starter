69b30ae720e714a29fca37a3ae1762c1ef88c253
ryan <ryanscode@163.com>
2022-07-24 12:42:55 +0800

    [0724] install axios and config interceptor

---
+++ src/api/request.ts
@@ -0,0 +1,42 @@
+/**
+ * axios实例创建及拦截器配置
+ */
+import axios from "axios";
+import store from "@/store";
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: process.env.BASE_API, // 接口baseUrl
+  timeout: 12000, // 超时时间
+});
+
+// 请求拦截器（reques interceptor）
+service.interceptors.request.use(
+  (config: any) => {
+    // Todo 可在此处配置为每个请求配置token
+    //这个config一定要return出去
+    return config;
+  },
+  (error) => {
+    // Todo 跳转到请求错误页面
+    Promise.reject(error);
+  }
+);
+
+// 响应拦截器（response interceptor）
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    if (res.status !== "200") {
+      // Todo 根据错误状态进行处理
+      return Promise.reject("error");
+    } else {
+      return response.data;
+    }
+  },
+  (error) => {
+    // Todo 错误处理，如弹窗提示
+    return Promise.reject(error);
+  }
+);
+export default service;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,12 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>{{ msg }}</p>
-    <a-button type="primary">新增</a-button>
+    <a-button type="primary" @click="getUserInfo()">新增</a-button>
   </div>
 </template>
 <script lang="ts">
 import { Button, ButtonGroup } from "ant-design-vue";
 import { Options, Vue } from "vue-class-component";
+import request from "../api/request"
 
 @Options({
   props: {
@@ -17,6 +18,12 @@ import { Options, Vue } from "vue-class-component";
   },
 })
 export default class HomeView extends Vue {
-  msg = 1111;
+  msg = "success";
+  // 获取用户名
+  getUserInfo() {
+    request.get("user/info", {}).then(res => {
+      console.log(res);
+    });
+  }
 }
 </script>
